e9dfbf9c008e0503ee837afac056c232fd425e55
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-02-20 22:05:37 +0500

    Optimization: make one write syscall in print_num

--- print.asm
+++ print.asm
@@ -5,6 +5,11 @@ section .data
 
 DEF_STR_DATA newline, 10
 
+section .bss
+
+print_num_buf resb 8
+print_num_buf_end equ $
+
 section .text
 
 global print
@@ -23,34 +28,36 @@ print:
 ; rax: number
 print_num:
 	push rbx
-	push rbp
-
-	mov rbp, rsp
+	push rsi
 
 	mov rbx, 10 ; for idiv
+	mov rcx, 0 ; counter for digits
 
-	.loop_get_chars:
+	.loop:
+		; divide [rdx:rax] by rbx
+		; rdx contains remainder - digit
 		mov rdx, 0
-		idiv rbx ; divide [rdx:rax] by rbx
+		idiv rbx
 
-		add dl, '0' ; add code of 0 to get ASCII char code
-		push rdx ; save new digit
+		; add code of 0 to get ASCII char code
+		add dl, '0'
+
+		; save digit in the first free cell in
+		; buffer from the end
+		inc rcx
+		mov rsi, print_num_buf_end
+		sub rsi, rcx
+		mov [rsi], dl ; save
 
 		cmp rax, 0
-		jne .loop_get_chars
+		jne .loop
 
-	.loop_print:
-		mov rax, rsp
-		mov rdx, 1
-		call print
+	mov rax, print_num_buf_end
+	sub rax, rcx
+	mov rdx, rcx
+	call print
 
-		add rsp, 8
-		cmp rsp, rbp
-		jne .loop_print
-
-	mov rsp, rbp
-
-	pop rbp
+	pop rsi
 	pop rbx
 	ret
 
